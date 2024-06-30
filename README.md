a1fcbd880b85467982b4cc5e4f9b202099ae304a
NPN <me@npn.am>
2024-06-30 09:14:29 +0700

    initial

---
+++ README.md
@@ -0,0 +1,37 @@
+# Công cụ phân tích ngữ pháp
+
+## Hướng dẫn cài đặt
+
+- Cài python3 + pip3
+- `pip3 install hanlp flask waitress`
+- `python3 hanlp-srv.py`
+
+## Gọi công cụ
+
+Thông qua curl:
+
+```sh
+curl -X POST http://localhost/mtl/1 -d "商品和服务。\n晓美焰来到北京立方庭参观自然语义科技公司"
+```
+
+Thông qua python:
+
+```py
+import requests
+
+url = "http://localhost/mtl/1"
+data = "商品和服务。\n晓美焰来到北京立方庭参观自然语义科技公司"
+headers = {"Content-Type": "text/plain"}
+
+response = requests.post(url, data=data, headers=headers)
+
+print(f"Status Code: {response.status_code}")
+print(f"Response: {response.json()}")
+```
+
+`1 2 3` tương ứng với 3 model Electra Small, Electra Base và Ernie Gram. Càng về sau thì độ chính sác càng cao.
+
+## Lưu ý:
+
+- Lần đầu tiên chạy sẽ khá lâu vì phải tải file model xuống.
+- Phải chia dòng theo từng câu văn mới đem lại tốc độ cao nhất.
---
+++ hanpl-srv.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import os, re, gc, json, hanlp, torch
+from flask import Flask, request
+
+TASKS = {}
+
+def load_task(kind):
+    if kind in TASKS:
+        return TASKS[kind]
+
+    if kind == '3':
+        mtl_task = hanlp.load(hanlp.pretrained.mtl.CLOSE_TOK_POS_NER_SRL_DEP_SDP_CON_ERNIE_GRAM_ZH)
+        del mtl_task['tok/coarse']
+
+    elif kind == '2':
+        mtl_task = hanlp.load(hanlp.pretrained.mtl.CLOSE_TOK_POS_NER_SRL_DEP_SDP_CON_ELECTRA_BASE_ZH)
+        del mtl_task['tok/coarse']
+
+    else:
+        mtl_task = hanlp.load(hanlp.pretrained.mtl.CLOSE_TOK_POS_NER_SRL_DEP_SDP_CON_ELECTRA_SMALL_ZH)
+        del mtl_task['tok/coarse']
+
+    del mtl_task['con']
+    del mtl_task['dep']
+    del mtl_task['sdp']
+    del mtl_task['srl']
+    del mtl_task['ner/pku']
+    del mtl_task['ner/ontonotes']
+    del mtl_task['pos/pku']
+    del mtl_task['pos/863']
+
+    TASKS[kind] = mtl_task
+    return mtl_task
+
+app = Flask(__name__)
+app.json.ensure_ascii = False
+
+@app.route("/mtl/<kind>", methods=['POST'])
+def parse_text(kind):
+    inp_data = request.get_data(as_text=True).splitlines()
+    mtl_data = load_task(kind)(inp_data)
+
+    torch.cuda.empty_cache()
+    gc.collect()
+
+    return mtl_data.to_json()
+
+@app.route("/tok/<kind>", methods=['POST'])
+def parse_toks(kind):
+    inp_data = request.get_data(as_text=True).splitlines()
+    mtl_data = load_task(kind)(inp_data)
+
+    torch.cuda.empty_cache()
+    gc.collect()
+
+    return mtl_data.to_json()
+
+## start app
+if __name__ == '__main__':
+    from waitress import serve
+    serve(app, port=5544)
